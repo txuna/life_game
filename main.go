1aa46dc1739cd80e6ebef8cd15539d985a54245e
txuna <b7712983@naver.com>
2024-04-15 13:35:06 +0900

    redis config

--- main.go
+++ main.go
@@ -7,12 +7,40 @@ import (
 	"server/network"
 )
 
+type DbConfig struct {
+	Type     string `json:"type"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Database string `json:"database"`
+	Protocol string `json:"protocol"`
+}
+
+type RedisConfig struct {
+}
+
 func main() {
 	netConfig, err := parseNetConfig()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	dbConfig, err := parseDbConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	redisConfig, err := parseRedisConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_ = redisConfig
+	_ = dbConfig
 
 	startLifeGameServer(netConfig)
 }
@@ -27,7 +55,37 @@ func parseNetConfig() (network.NetConfig, error) {
 	defer file.Close()
 
 	jsonParser := json.NewDecoder(file)
-	jsonParser.Decode(netConfig)
+	jsonParser.Decode(&netConfig)
 
 	return netConfig, err
+}
+
+func parseDbConfig() (DbConfig, error) {
+	var dbConfig DbConfig
+	file, err := os.Open("./config/db_config.json")
+	if err != nil {
+		return dbConfig, nil
+	}
+
+	defer file.Close()
+
+	jsonParser := json.NewDecoder(file)
+	jsonParser.Decode(&dbConfig)
+
+	return dbConfig, nil
+}
+
+func parseRedisConfig() (RedisConfig, error) {
+	var redisConfig RedisConfig
+	file, err := os.Open("./config/redis_config.json")
+	if err != nil {
+		return redisConfig, err
+	}
+
+	defer file.Close()
+
+	jsonParser := json.NewDecoder(file)
+	jsonParser.Decode(&redisConfig)
+
+	return redisConfig, nil
 }
